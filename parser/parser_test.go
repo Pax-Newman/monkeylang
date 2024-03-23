6111e90aa7d8236cc64701633ec162891d9e9841
Pax-Newman <newmanp@wwu.edu>
2024-03-22 19:05:20 -0700

    added integer parsing

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -136,3 +136,34 @@ func TestIdentifierExpression(t *testing.T) {
 		t.Errorf("ident.TokenLiteral not `foobar`. Got `%s`", ident.TokenLiteral())
 	}
 }
+
+func TestIntegerLiteralExpression(t *testing.T) {
+	input := "5;"
+
+	l := lexer.New(input)
+	p := New(l)
+	program := p.Parse()
+	checkParserErrors(t, p)
+
+	if len(program.Statements) != 1 {
+		t.Fatalf("Program does not have 1 statement. Got `%d`", len(program.Statements))
+	}
+
+	stmt, ok := program.Statements[0].(*ast.ExpressionStatement)
+	if !ok {
+		t.Fatalf("program.Statements[0] is not *ast.ExpressionStatement. Got `%T`", program.Statements[0])
+	}
+
+	literal, ok := stmt.Expression.(*ast.IntegerLiteral)
+	if !ok {
+		t.Fatalf("Expression is not *ast.IntegerLiteral. Got `%T`", stmt.Expression)
+	}
+
+	if literal.Value != 5 {
+		t.Errorf("literal.Value is not `5`. Got `%d`", literal.Value)
+	}
+
+	if literal.TokenLiteral() != "5" {
+		t.Errorf("literal.TokenLiteral not `5`. Got `%s`", literal.TokenLiteral())
+	}
+}
